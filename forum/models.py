b62f3739ff2f4f6ff388708cab5dd357bbe13269
PELLAN Florian <florian.pellan@live.fr>
2021-01-08 00:31:03 +0100

    First basic Models implementation for the forum part of stack undeflow

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Thread(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Thread by {self.author}"
+
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.SET_NULL)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Post by {self.author} for Thread {self.thread}"
+
+
+class Karma(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    karma = models.BooleanField()
+
+    def __str__(self):
+        return f"Karma is {self.karma} by {self.author} for Post {self.post}"
